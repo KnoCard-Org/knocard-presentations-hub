beb9d0889767152ef60b46f9ead3f90c766bc001
azdine <azdinebouali2002@gmail.com>
2025-03-02 23:51:32 +0000

    fixed password page

--- app/password/page.tsx
+++ app/password/page.tsx
@@ -23,9 +23,18 @@ async function page() {
         </div>
         <div className='md:w-1/2 w-full h-full flex flex-col items-center justify-center'>
           <PasswordForm />
+          <p className='bg-blue-800 text-white px-5
+          py-2 rounded-lg mt-5 max-w-xs mx-auto text-sm  text-balance'>
+            This page is password-protected. Please contact the person who shared KnoCard with you for access and a guided presentation.
+          </p>
           <div className='mt-auto mb-10'>
-            <p>powered by </p>
-            <p className='font-bold text-xl'>POP Presentation</p>
+
+            <Image
+              src={'/pop-presentations-logo.png'}
+              alt='Pop Presentations'
+              width={200}
+              height={40}
+            />
           </div>
         </div>
       </div >
